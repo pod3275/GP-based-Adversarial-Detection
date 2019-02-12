0ca4fd5a7a71c3764efc319074fa82315103eee9
이상헌 <pod3275@gmail.com>
2019-02-12 19:20:15 +0900

    Add files via upload
    code simplification

--- Detector.py
+++ Detector.py
@@ -14,14 +14,21 @@ from tqdm import tqdm
 from keras.models import load_model
 # ----------------------------------------------------------------------
 # Hyperparameter Setting
-num_data_in_class =10
-iteration = 40
-train_dataset = 'Cleverhans/CIFAR10/' + 'JSMA'
-file_name = 'CIFAR10_JSMA_100_Detector.txt'
+num_data_in_class = 10
+iteration = 30
+# total number of data = 위 * 10
+
+data_name = 'CIFAR10'
+attack_name = 'DeepFool'
+
+train_dataset = data_name + '/' + attack_name
+
+f = open(data_name + '_' + attack_name + '_' + str(num_data_in_class*10) + '_Detector.txt', 'w')
+f.write(data_name + '_' + attack_name + '_' + str(num_data_in_class*10) + '_Detector\n')
 
 keras.backend.set_learning_phase(0)
-kmodel = load_model('saved_models/cifar10_ResNet32v1_model_no_sub_mean.091.h5')
-# kmodel = load_model('saved_models/MNIST_model.016.h5')
+if data_name == 'CIFAR10': kmodel = load_model('saved_models/cifar10_ResNet32v1_model.h5')
+elif data_name == 'MNIST': kmodel = load_model('saved_models/MNIST_model.h5')
 
 # ----------------------------------------------------------------------
 def preprocess_input2(x):
@@ -130,14 +137,15 @@ def model_prediction(model, x, y, epoch):
     precision = tp/(tp+fp)
     recall = tp/(tp+fn)
     F1 = 2* (precision*recall) / (precision+recall)
+    
     f.write('Iteration : ' + str(epoch) + '\n')
     f.write('Accuracy: ' + str((tp+tn)/(tp+tn+fp+fn)*100) +'%\n')
     f.write('Precision: ' + str(precision)+',  Recall: ' + str(recall)+'\nF1 Score: ' + str(F1) +'\n\n')
+    print('Accuracy: ' + str((tp+tn)/(tp+tn+fp+fn)*100) +'%\n')
     # f.write('테스트 데이터 수: '+ str(len(x))+ ',  바뀐 개수: '+ str(train_count)  +'\n\n')
     
 # ----------------------------------------------------------------------
 if __name__ == '__main__': 
-    f = open(file_name, 'w')
     print('\n===============Data Load================\n')
     adv, clean, _, _, _ = load_img(train_dataset)
     _, _, adv_test, clean_test, total = load_img(train_dataset)
@@ -145,21 +153,20 @@ if __name__ == '__main__':
     x_train, y_train = get_last_hidden_output(adv, clean)
     print('\n==========Test Data Calculate===========\n')
     x_test, y_test = get_last_hidden_output(adv_test, clean_test)
-    f.write('Training data:'+str(len(x_train))+'\n')
-    f.write('Test data:'+ str(len(x_test))+'\n')
+
+    f.write('Training data: '+str(len(x_train))+'\n')
+    f.write('Test data: '+ str(len(x_test))+'\n')
+    
     #draw the latent function value
-    iterate = 0
-    iterate = iterate+1
-    for iterate in range(5):
+    for iterate in range(3):
         print('Iteration : ', iterate+1)
-        print('\n===============Model Load================\n')
         
         k = GPy.kern.Matern52(input_dim=10, variance=1.)
         m = GPy.models.SparseGPClassification(x_train, y_train, kernel=k)
         
         print('\n=============Model Training==============\n')
         for i in range(iteration):
-            m.optimize('bfgs', max_iters=1000)
+            m.optimize('lbfgs', max_iters=1000)
             
         model_prediction(m, x_test, y_test, iterate)
     
--- attack.py
+++ attack.py
@@ -12,14 +12,22 @@ import os
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
-from keras.datasets import mnist
-from keras.datasets import cifar10
+from keras.datasets import mnist, cifar10
 from cleverhans.attacks import BasicIterativeMethod, CarliniWagnerL2, SaliencyMapMethod, FastGradientMethod, DeepFool
 from cleverhans.utils_keras import KerasModelWrapper
 from cleverhans.utils_tf import model_eval
 from keras.models import load_model
 from keras import backend as K
+from matplotlib import pyplot as plt
 
+
+data_name = 'CIFAR10'
+attack_name = 'DeepFool'
+save_loc = '/Data/' + data_name + '/' + attack_name
+
+
+f = open(data_name + '_' + attack_name + '_Classifier.txt', 'w')
+f.write(data_name + '_' + attack_name + '_Classifier\n')
 
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
@@ -27,8 +35,64 @@ config.gpu_options.allow_growth = True
 sess = tf.Session(config=config)
 keras.backend.set_session(sess)
 
-data_name = 'MNIST'
-attack_name = 'FGSM'
+def save_images(kmodel, adv_x, x_test, y_test, preds1, save_loc_add):
+    count_class_save = np.zeros(10)
+    count_class_save = count_class_save.astype('int32')
+    origin_count=0
+    adv_count=0
+    adv_result = []
+    buffer = []
+    buffer2 = []
+    # i=0
+    for i in tqdm(range(len(x_test))):
+        x_img_input = x_test[i][np.newaxis,:,:,:]
+        y_input=[]
+        y_input.append(preds1[i])
+
+        if attack_name == 'BIM':
+            y_input = keras.utils.to_categorical(y_input, 10)
+
+        adv_x_eval = adv_x.eval(session = sess, feed_dict={x:x_img_input/255, y:y_input})
+        adv_x_eval = adv_x_eval*255
+        preds2 = np.argmax(kmodel.predict(adv_x_eval/255), axis=1)
+        
+        '''
+        plt.figure()
+        plt.subplot(1, 3, 1)
+        plt.imshow(adv_x_eval[0] / 255)  # division by 255 to convert [0, 255] to [0, 1]
+        plt.axis('off')
+        '''
+        
+        adv_result.append(preds2[0])
+        
+        if not(os.path.isdir(os.getcwd() + save_loc_add + '/' + str(y_test[i][0]) + '/')):
+                os.makedirs(os.path.join(os.getcwd() + save_loc_add + '/' + str(y_test[i][0]) + '/'))
+                             
+        if preds1[i][0] == y_test[i][0] and preds2[0] != y_test[i][0]:
+            np.save(os.getcwd() + save_loc_add + '/' + str(y_test[i][0]) + '/' 
+                    + str(count_class_save[y_test[i][0]])
+                    + '_origin.npy', x_test[i])
+            
+            np.save(os.getcwd() + save_loc_add + '/' + str(y_test[i][0]) + '/'
+                    + str(count_class_save[y_test[i][0]])
+                    + '_adv.npy', adv_x_eval[0])
+
+            count_class_save[y_test[i][0]] = count_class_save[y_test[i][0]] + 1
+        
+        if preds1[i][0] == y_test[i][0]:
+            origin_count +=1
+            
+        if preds2[0] == y_test[i][0]:
+            adv_count +=1
+        
+        buffer.append(preds2[0])
+        buffer2.append(preds1[i][0])
+            
+    f.write('Count_Class_Save: ' + str(count_class_save) + '\n')
+    f.write('Origin Accuracy: ' + str(origin_count/len(x_test) * 100) + '%\n')
+    f.write('Adv Accuracy: ' + str(adv_count/len(x_test) * 100) + '%\n\n')
+    #f.write('Origin class: ' + str(buffer2[0:100]) + '\n')
+    #f.write('Adv class: ' + str(buffer[0:100]) + '\n')
 
 
 if data_name == 'MNIST':
@@ -69,83 +133,118 @@ elif data_name == 'CIFAR10':
 
 
 #Placeholder
-x = tf.placeholder(tf.float32, shape=(None, img_rows, img_cols,
-                                      nchannels))
+x = tf.placeholder(tf.float32, shape=(None, img_rows, img_cols, nchannels))
 y = tf.placeholder(tf.float32, shape=(None, nb_classes))
 
+if attack_name == 'BIM':
+    y = tf.placeholder(tf.float32, shape=(None, 10))
+    
+preds1 = np.argmax(kmodel.predict(x_test/255), axis=1)
+preds1 = preds1.reshape(preds1.shape[0], 1)
 
+
+#Attack
 if attack_name == 'FGSM':
     attack = FastGradientMethod(wrap, sess=sess)
     eps = 0
-    batch_size = 128
-    eval_par = {'batch_size': batch_size}
     
-    preds1 = np.argmax(kmodel.predict(x_test/255), axis=1)
-    preds1 = preds1.reshape(preds1.shape[0], 1)
-    
-    f = open(data_name + '_' + attack_name + '_Classifier.txt', 'w')
-    
-    if data_name == 'MNIST': 
-        for ep in tqdm(range(5)):
+    if data_name == 'MNIST':
+        for ep in range(5):
             eps = eps + 0.1
             params = {'eps': eps,
                       'clip_min': 0.,
                       'clip_max': 1.}
             adv_x = attack.generate(x, **params)
             
-            count_class_save = np.zeros(10)
-            count_class_save = count_class_save.astype('int32')
-            origin_count=0
-            adv_count=0
-            
-            adv_result = []
-        
             print('Epsilon:', eps)
-            
-            for i in tqdm(range(len(x_test))):
-                x_img_input = x_test[i][np.newaxis,:,:,:]
-                
-                y_input=[]
-                y_input.append(preds1[i])
-                y_input = keras.utils.to_categorical(y_input, 10)
-                
-                adv_x_eval = adv_x.eval(session = sess, feed_dict={x:x_img_input/255, y:y_input})
-                adv_x_eval = adv_x_eval*255
-                preds2 = np.argmax(kmodel.predict(adv_x_eval/255), axis=1)
-                
-                adv_result.append(preds2[0])
-                
-                if not(os.path.isdir(os.getcwd() + '/Data/MNIST/FGSM_e' + str(eps) + '/' + str(y_test[i][0]) + '/')):
-                        os.makedirs(os.path.join(os.getcwd() + '/Data/MNIST/FGSM_e' + str(eps) + '/' + str(y_test[i][0]) + '/'))
-                                     
-                if preds1[i][0] == y_test[i][0] and preds2[0] != y_test[i][0]:
-                    np.save(os.getcwd() + '/Data/MNIST/FGSM_e' + str(eps) + '/' + str(y_test[i][0]) + '/' 
-                            + str(count_class_save[y_test[i][0]])
-                            + '_origin.npy', x_test[i])
-                    np.save(os.getcwd() + '/Data/MNIST/FGSM_e' + str(eps) + '/' + str(y_test[i][0]) + '/'
-                            + str(count_class_save[y_test[i][0]])
-                            + '_adv.npy', adv_x_eval[0])
-        
-                    count_class_save[y_test[i][0]] = count_class_save[y_test[i][0]] + 1
-                
-                if preds1[i][0] == y_test[i][0]:
-                    origin_count+=1
-                if preds2[0] == y_test[i][0]:
-                    adv_count+=1
-                    
             f.write('Epsilon: ' + str(eps) + '\n')
-            f.write('Origin Accuracy: ' + str(origin_count/len(x_test) * 100) + '%\n')
-            f.write('Adv Accuracy: ' + str(adv_count/len(x_test) * 100) + '%\n\n')
+                        
+            save_images(kmodel, adv_x, x_test, y_test, preds1, save_loc + '_e' + str(eps))
             
-
+    if data_name == 'CIFAR10':
+        for ep in range(10):
+            eps = eps + 1
+            params = {'eps': eps/255,
+                      'clip_min': 0.,
+                      'clip_max': 1.}
+            adv_x = attack.generate(x, **params)
+            
+            print('Epsilon:', eps)
+            f.write('Epsilon: ' + str(eps) + '\n')
+                        
+            save_images(kmodel, adv_x, x_test, y_test, preds1, save_loc + '_e' + str(eps))
+           
+            
 #attack_JSMA
-attack = SaliencyMapMethod(wrap, sess=sess)
+if attack_name == 'JSMA':
+    attack = SaliencyMapMethod(wrap, sess=sess)
+    params = {'clip_min': 0., 'clip_max': 1.}
+    adv_x = attack.generate(x, **params)
+    save_images(kmodel, adv_x, x_test, y_test, preds1, save_loc)
+
 
 #attack_BIM
-attack = BasicIterativeMethod(wrap, sess=sess)
+if attack_name == 'BIM':
+    attack = BasicIterativeMethod(wrap, sess=sess)
+    eps = 0
+    
+    if data_name == 'MNIST':
+        for ep in range(5):
+            eps = eps + 0.1
+            params = {'eps': eps,
+                      'eps_iter': eps/10,
+                      'nb_iter': 10,
+                      'y': y,
+                      'clip_min': 0.,
+                      'clip_max': 1.}
+            adv_x = attack.generate(x, **params)
+            adv_x = tf.stop_gradient(adv_x)
+            
+            print('Epsilon:', eps)
+            f.write('Epsilon: ' + str(eps) + '\n')
+                        
+            save_images(kmodel, adv_x, x_test, y_test, preds1, save_loc + '_e' + str(eps))
+            
+    if data_name == 'CIFAR10':
+        for ep in range(10):
+            eps = eps + 1
+            params = {'eps': eps/255,
+                      'eps_iter': eps/255/10,
+                      'nb_iter': 10,
+                      'y': y,
+                      'clip_min': 0.,
+                      'clip_max': 1.}
+            adv_x = attack.generate(x, **params)
+            adv_x = tf.stop_gradient(adv_x)
+            
+            print('Epsilon:', eps)
+            f.write('Epsilon: ' + str(eps) + '\n')
+                 
+            save_images(kmodel, adv_x, x_test, y_test, preds1, save_loc + '_e' + str(eps))
+
 
 #attack_DeepFool
-attack = DeepFool(wrap, sess=sess)
+if attack_name == 'DeepFool':
+    attack = DeepFool(wrap, sess=sess)
+    params = {'nb_candidate': 10,
+              'max_iter': 100,
+              'clip_min': 0.,
+              'clip_max': 1.}
+    adv_x = attack.generate(x, **params)
+    save_images(kmodel, adv_x, x_test, y_test, preds1, save_loc)
+
 
 #attack_CW
-attack = CarliniWagnerL2(wrap, sess=sess)
+if attack_name == 'CW':
+    attack = CarliniWagnerL2(wrap, sess=sess)
+    params = {'batch_size':1,
+              'max_iterations':1000,
+              'binary_search_steps':9,
+              'initial_const':1e-3,
+              'learning_rate':5e-3,
+              'clip_min': 0.,
+              'clip_max': 1.}
+    adv_x = attack.generate(x, **params)
+    save_images(kmodel, adv_x, x_test, y_test, preds1, save_loc)
+
+f.close()
