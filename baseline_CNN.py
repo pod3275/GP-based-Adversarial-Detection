2d3a9fc71958e564b52230e5dfba073a4f654d7a
이상헌 <pod3275@gmail.com>
2019-09-05 20:15:55 +0900

    Add files via upload

--- baseline_CNN.py
+++ baseline_CNN.py
@@ -1,22 +1,37 @@
-import os
-# supress tensorflow logging other than errors
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-data_name = 'MNIST'
-attack_name = 'CW'
-num_data_in_class = 30
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  5 20:12:06 2019
 
+@author: lawle
+"""
+
+import os
 import numpy as np
 import tensorflow as tf
 
 from keras import backend as K
-from keras.models import Sequential, load_model
+from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D, LeakyReLU
-from keras.callbacks import EarlyStopping
-import matplotlib.pyplot as plt
 from tqdm import tqdm
+import timeit
+
+# supress tensorflow logging other than errors
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string('dataset', 'MNIST', 'Training dataset name')
+flags.DEFINE_string('attack', 'FGSM', 'Adversarial attack name')
+flags.DEFINE_integer('num_data_in_class', 30, 'Number of adversarial example per one class used for detector')
+
+
+print("Compared Baseline Model. (DeepLearning-based binary detector model)")
+print("Detect", FLAGS.attack, "attack on", FLAGS.dataset, "CNN model with", str(FLAGS.num_data_in_class*10), "adversarial examples.")
+
 
 def load_img():
-    location = os.getcwd() + '/Data/' + data_name + '/' + attack_name
+    location = os.getcwd() + '/' + FLAGS.dataset + '/' + FLAGS.attack
     file_list = os.listdir(location)
         
     count_data = np.zeros(10)
@@ -38,7 +53,7 @@ def load_img():
             img_plx1 = np.asarray(img, dtype='float32')
         
             if 'adv' in file_name:
-                if count_data[int(i)] < 2* num_data_in_class:
+                if count_data[int(i)] < 2* FLAGS.num_data_in_class:
                     adv_img.append(img_plx1)
                     count_data[int(i)] = count_data[int(i)]+1
                     clean_y.append(int(i))
@@ -46,7 +61,7 @@ def load_img():
                     adv_img_test.append(img_plx1)
                             
             if 'origin' in file_name:
-                if count_data[int(i)] < 2* num_data_in_class:
+                if count_data[int(i)] < 2* FLAGS.num_data_in_class:
                     clean_img.append(img_plx1)
                     count_data[int(i)] = count_data[int(i)]+1
                 else:
@@ -61,15 +76,15 @@ def load_img():
                                                   
     return adv_img, clean_img, adv_img_test, clean_img_test, clean_y, total
 
-f = open(data_name + '_' + attack_name + '_' + str(num_data_in_class*10) + '.txt', 'w')
+f = open(FLAGS.dataset + '_' + FLAGS.attack + '_' + str(FLAGS.num_data_in_class*10) + '_Baseline.txt', 'w')
 
-if data_name == 'MNIST':
+if FLAGS.dataset == 'MNIST':
     img_rows = 28
     img_cols = 28
     img_chan = 1
     nb_classes = 10
 
-elif data_name == 'CIFAR10':
+elif FLAGS.dataset == 'CIFAR10':
     img_rows = 32
     img_cols = 32
     img_chan = 3
@@ -81,6 +96,7 @@ K.set_session(sess)
 
 X_train_adv, X_train, X_test_adv, X_test, clean_y, count = load_img()
 
+# CNN-based adversarial detector - Binary classification
 print('\nPreparing clean/adversarial mixed dataset')
 X_all_train = np.vstack([X_train, X_train_adv])
 y_all_train = np.vstack([np.zeros([X_train.shape[0], 1]),
@@ -99,7 +115,9 @@ y_all_test = y_all_test[ind]
 
 for c in range(5):
     print('\nBuilding model')
-    if data_name == 'MNIST':
+    
+    # MNIST adversarial detector CNN model
+    if FLAGS.dataset == 'MNIST':
         model = Sequential([
             Conv2D(32, (3, 3), input_shape=input_shape),
             Activation('relu'),
@@ -115,16 +133,20 @@ for c in range(5):
             Activation('sigmoid')])
             
         model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    
+        start = timeit.default_timer()
         model.fit(X_all_train, y_all_train, epochs=2, validation_split=0)
-    
+        end = timeit.default_timer()
+        f.write(str(end - start) + ' seconds\n')
         f.write('Iteration : ' + str(c+1))
         print('Iteration : ' + str(c+1))
+        
         score = model.evaluate(X_all_test, y_all_test)
+        
         print('\nloss: {0:.4f} acc: {1:.4f}\n'.format(score[0], score[1]))
         f.write('\nloss: {0:.4f} acc: {1:.4f}\n'.format(score[0], score[1]))
     
-    elif data_name == 'CIFAR10':
+    # CIFAR10 adversarial detector CNN model
+    elif FLAGS.dataset == 'CIFAR10':
         model = Sequential([
             Conv2D(32, (3, 3), padding='same', input_shape=input_shape),
             LeakyReLU(alpha=0.2),
@@ -145,9 +167,10 @@ for c in range(5):
             Activation('sigmoid')])
 
         model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    
+        start = timeit.default_timer()        
         model.fit(X_all_train, y_all_train, epochs=7, validation_split=0)
-    
+        end = timeit.default_timer()
+        f.write(str(end - start) + ' seconds\n')  
         f.write('Iteration : ' + str(c+1))
         print('Iteration : ' + str(c+1))
         score = model.evaluate(X_all_test, y_all_test)
